0fb5e6b0f141386f9558e121b5b99720954f6f48
Jonson <luizlindonjonson@yahoo.com.br>
2022-05-05 10:31:28 -0300

    instalando o react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>OLÁ ESSE É MEU PRIMEIRO COMANDO EM REACT.</h1>
+     <p>Meu primeiro REACT</p>
+    </div>
+  );
+}
+
+export default App;
