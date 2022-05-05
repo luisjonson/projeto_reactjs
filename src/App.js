6d0f3368fd8d9dcb3948dca8c0b604bb1eea499b
Jonson <luizlindonjonson@yahoo.com.br>
2022-05-05 10:54:33 -0300

    Entendendo o Jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const nome ='Jonson'
+  const newName = nome.toUpperCase()
+
+  function sun(a,b){
+    return a + b
+  }
+
+  const url =  'https://via.placeholder.com/100'
   return (
     <div className="App">
      <h1>OLÁ ESSE É MEU PRIMEIRO COMANDO EM REACT.</h1>
-     <p>Meu primeiro REACT</p>
+     <p>Olá, {newName}</p>
+     <p>soma: {sun(1,4)}</p>
+     <img src={url} alt="Goolge"/>
     </div>
   );
 }
