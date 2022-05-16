7ac218caf5890ecbc423cc8983cf3088f3c0dd46
Jonson <luizlindonjonson@yahoo.com.br>
2022-05-16 18:58:28 -0300

    Curso React: Inserindo CSS no React(CSS modules) - #06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 import './App.css';
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
   const url =  'https://via.placeholder.com/100'
   return (
     <div className="App">
+      <Frase/>
+      <h1>Testando CSS</h1>
       <SayMyName name = "Jonson"/>
       <Pessoa nome="Jonson" idade="33" profissao="Programador" foto="https://via.placeholder.com.150"/>
     </div>
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,4 +1,4 @@
-import Frase from "../Frase"
+import Frase from "./Frase"
 function HelloWorld(){
     return (
         <div>
