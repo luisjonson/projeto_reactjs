b754279d6e144102b3342fc5cd8d11a7ea536112
Jonson <luizlindonjonson@yahoo.com.br>
2022-05-16 20:03:49 -0300

    Curso React: Eventos no React (onClick, onChange e onSubmit) - #09

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
@@ -13,11 +10,12 @@ function App() {
   const url =  'https://via.placeholder.com/100'
   return (
     <div className="App">
-      <Frase/>
-      <h1>Testando CSS</h1>
-      <SayMyName name = "Jonson"/>
-      <Pessoa nome="Jonson" idade="33" profissao="Programador" foto="https://via.placeholder.com.150"/>
-      <List/>
+      <h1>Testando Eventos</h1>
+      <Evento numero= "1"/>
+      <Evento numero= "2"/>
+      
+      <Form/>
+      
     </div>
   );
 }
